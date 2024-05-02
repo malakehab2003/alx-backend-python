a0cba6b60ae5d8f7212d1ae91c1197a6142cc84e
malakehab2003 <lokaehab2003@gmail.com>
2024-05-03 02:53:29 +0300

    task 8

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+""" Implement make_multiplier return function """
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """ make_multiplier take float number and return function """
+    return lambda n: n * multiplier
