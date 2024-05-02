64dbb57af89eabc1be934952bf3ec88ec23dc9a9
malakehab2003 <lokaehab2003@gmail.com>
2024-05-03 02:37:01 +0300

    task 7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Implement to_kv function which returned parameters as tuple """
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
+    """ to_kv take str and float or int and return tuple """
+    return (k, float(v) ** 2)
