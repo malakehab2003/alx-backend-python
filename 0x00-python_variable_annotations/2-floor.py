f162591bf45ae16820b32dadd9c8620452933022
malakehab2003 <lokaehab2003@gmail.com>
2024-05-03 01:48:49 +0300

    task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Implement floor function return floored number"""
+
+
+def floor(n: float) -> int:
+    """ floor function take float number and count its floor """
+    return int(n)
