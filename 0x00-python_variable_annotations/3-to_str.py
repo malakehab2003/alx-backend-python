2574b630d6ef64a993131d50d33573aa632a3287
malakehab2003 <lokaehab2003@gmail.com>
2024-05-03 01:51:45 +0300

    task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Implement to_str function to return a float to string """
+
+
+def to_str(n: float) -> str:
+    """ to_str function take a float number and convert it into string """
+    return str(n)
