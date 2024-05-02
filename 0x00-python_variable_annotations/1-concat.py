460f308a270f703b89197077f740d7112dce2d95
malakehab2003 <lokaehab2003@gmail.com>
2024-05-03 01:41:41 +0300

    task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Implement concat function to concatinate two strings """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ concat function take two stirngs and return them concatinated """
+    return str1 + str2
