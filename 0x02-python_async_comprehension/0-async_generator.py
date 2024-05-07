ebbb505afb19fd19a20fcaa5cdf29a355b790bab
malakehab2003 <lokaehab2003@gmail.com>
2024-05-07 19:03:42 +0300

    task 0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Create async_generator function """
+import random
+import typing
+import asyncio
+
+
+async def async_generator() -> typing.AsyncGenerator[float, None]:
+    """ Wait one second and yield random number """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
