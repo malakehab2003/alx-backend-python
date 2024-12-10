9280f259d07e459832f270577105225446c0df8d
malakehab2003 <112804762+malakehab2003@users.noreply.github.com>
2024-12-10 14:56:13 +0200

    fix 0-databaseconnection.py add init to make code more usable

--- python-context-async-perations-0x02/0-databaseconnection.py
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -2,15 +2,26 @@
 import mysql.connector
 
 class DatabaseConnection:
+  def __init__(self, host='localhost', user='root', password='root', database='ALX_prodev'):
+        """
+        Initialize with database connection parameters.
+        """
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+        self.connection = None
+
+
   def __enter__(self):
     """
       connect to database to perform the queries
     """
     self.connection = mysql.connector.connect(
-      host='localhost',
-      user='root',
-      password='root',
-      database='ALX_prodev'
+      host=self.host,
+      user=self.user,
+      password=self.password,
+      database=self.database
     )
 
     return self.connection
