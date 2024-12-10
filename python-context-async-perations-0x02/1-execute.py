5c8a92e6ce4a4e60a096a7ea58de49a466dc7381
malakehab2003 <112804762+malakehab2003@users.noreply.github.com>
2024-12-10 15:34:16 +0200

    add 1-execute.py to execute command with params

---
+++ python-context-async-perations-0x02/1-execute.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+import mysql.connector
+
+
+class ExecuteQuery:
+  def __init__(
+      self,
+      query='SELECT * FROM users WHERE age > ',
+      params=(25,),
+      host='localhost',
+      user='root',
+      password='root',
+      database='alx_prodev',
+  ):
+    """
+      create the constructor
+    """
+    self.query = query
+    self.params = params
+    self.connection = None
+    self.cursor = None
+    self.host = host
+    self.user = user
+    self.password = password
+    self.database = database
+
+  def __enter__(self):
+    """
+      create the context manager enterance
+    """
+    self.connection = mysql.connector.connect(
+      host=self.host,
+      user=self.user,
+      password=self.password,
+      database=self.database
+    )
+    self.cursor = self.connection.cursor()
+    return self
+  
+  def execute(self):
+    """
+    execute the command and return the result
+    """
+    self.cursor.execute(self.query, self.params)
+    result = self.cursor.fetchall()
+    return result
+  
+  def __exit__(self, exc_type, exc_value, traceback):
+    """
+      close the context manager
+    """
+    self.cursor.close()
+    self.connection.close()
+
+with ExecuteQuery() as e:
+  results = e.execute()
+  for result in results:
+    print(result)
