06de380dc410ec4c430b039a58c30c0853be06f0
malakehab2003 <112804762+malakehab2003@users.noreply.github.com>
2024-12-21 15:35:11 +0200

    fix settings.py

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -38,10 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.permissions.IsAuthenticated',
-    'DEFAULT_AUTHENTICATION_CLASSES',
-    'rest_framework.authentication.SessionAuthentication',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
