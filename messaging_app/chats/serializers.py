69e42ca8fab240dfee86168a6167ec82b9d6a740
malakehab2003 <112804762+malakehab2003@users.noreply.github.com>
2024-12-21 19:47:20 +0200

    fix serializers.py

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -2,15 +2,27 @@ from rest_framework import serializers
 from .models import Conversation, Message, User
 
 class UserSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField()
     class Meta:
         model = User
         fields = '__all__'
 
+    def get_full_name(self, obj):
+        """Returns the full name of the user."""
+        return f"{obj.first_name} {obj.last_name}"
+
 
 class MessageSerializer(serializers.ModelSerializer):
+    sender_name = serializers.CharField(source='sender.first_name', read_only=True)
     class Meta:
         model = Message
         fields = '__all__'
+
+    def validate_message_body(self, value):
+        """Custom validation for the message body."""
+        if len(value) < 1:
+            raise serializers.ValidationError("Message body cannot be empty.")
+        return value
 
 
 class ConversationSerializer(serializers.ModelSerializer):
