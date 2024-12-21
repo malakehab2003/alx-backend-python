579bf691f35c237e0bed43749c8c064e120f4e89
malakehab2003 <112804762+malakehab2003@users.noreply.github.com>
2024-12-21 15:26:54 +0200

    add messaging_app to create messaging app using django

--- python-decorators-0x01/add_users.py
+++ python-decorators-0x01/add_users.py
@@ -6,22 +6,25 @@ conn = sqlite3.connect('users.db')
 # Create a cursor object
 cursor = conn.cursor()
 
+cursor.execute('DROP TABLE IF EXISTS users')
+
 # Create the `users` table
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY,
     name TEXT NOT NULL,
-    email TEXT NOT NULL
+    email TEXT NOT NULL,
+    age DECIMAL(10) NOT NULL
 )
 ''')
 
 # Insert sample data
 cursor.executemany('''
-INSERT INTO users (id, name, email) VALUES (?, ?, ?)
+INSERT INTO users (id, name, email, age) VALUES (?, ?, ?, ?)
 ''', [
-    (1, 'John Doe', 'john.doe@example.com'),
-    (2, 'Jane Smith', 'jane.smith@example.com'),
-    (3, 'Alice Johnson', 'alice.johnson@example.com')
+    (1, 'John Doe', 'john.doe@example.com', 41),
+    (2, 'Jane Smith', 'jane.smith@example.com', 20),
+    (3, 'Alice Johnson', 'alice.johnson@example.com', 45)
 ])
 
 # Commit and close the connection
