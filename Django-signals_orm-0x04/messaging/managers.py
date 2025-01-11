8ba890ddb49eeb900fd7e0249d4c2a094a6ca375
malakehab2003 <112804762+malakehab2003@users.noreply.github.com>
2025-01-11 22:19:26 +0200

    add unread messages

--- Django-signals_orm-0x04/messaging/managers.py
+++ Django-signals_orm-0x04/messaging/managers.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class UnreadMessagesManager(models.Manager):
-    def get_unread_messages(self, user):
-        return self.filter(receiver=user, read=False).only('id', 'sender', 'content', 'timestamp')
+    def unread_for_user(self, user):
+        return self.filter(receiver=user, read=False)
