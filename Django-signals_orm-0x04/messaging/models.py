206553019ab316d31ae2f4d1eabab8aa66a68c05
malakehab2003 <112804762+malakehab2003@users.noreply.github.com>
2025-01-11 20:31:35 +0200

    add edited by to MessageHistory model

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -19,3 +19,4 @@ class MessageHistory(models.Model):
     message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='history')
     old_content = models.TextField()
     edited_at = models.DateTimeField(auto_now_add=True)
+    edited_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='edited_message')
